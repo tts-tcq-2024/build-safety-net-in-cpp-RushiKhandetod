9ba8976481e725f202a95db371081a80d350647c
RushiKhandetod <90644030+RushiKhandetod@users.noreply.github.com>
2024-07-08 13:57:59 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,12 +1,44 @@
 #include <gtest/gtest.h>
-#include "Soundex.h"
+#include <string>
 
+// Function to generate Soundex code (you need to implement this)
+std::string generateSoundex(const std::string& name) {
+    // Implement the Soundex algorithm here
+    return "";
+}
 
-TEST(SoundexTest, HandlesEmptyString) {
+// Test fixture class
+class SoundexTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Code here will be called immediately after the constructor (right before each test).
+    }
+
+    void TearDown() override {
+        // Code here will be called immediately after each test (right before the destructor).
+    }
+};
+
+// Test cases
+TEST_F(SoundexTest, EmptyString) {
     EXPECT_EQ(generateSoundex(""), "");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
+TEST_F(SoundexTest, SingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST_F(SoundexTest, SimpleCases) {
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+    EXPECT_EQ(generateSoundex("Rubin"), "R150");
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Ashcroft"), "A261");
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+}
+
+// Main function to run all the tests
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
